74ac4467736f34c9de0654b2c0485609a379ecaf
zhouyf <376298333@qq.com>
2019-07-05 15:33:53 +0800

    List

---
+++ Chapter 4 - Lists/Lists.py
@@ -0,0 +1,9 @@
+
+# 遍历 List
+supplies = ['pens', 'staplers', 'flame-throwers', 'binders']
+for i in range(len(supplies)):
+    print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+# 多重赋值
+cat = ['fat', 'black', 'loud']
+size, color, disposition = cat
