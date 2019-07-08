49f22a44a6bac32b4b5e7363100ff2a8057b7507
zhouyf <376298333@qq.com>
2019-07-08 10:50:59 +0800

    Dictionaries

---
+++ Chapter 5 - Dictionaries and Structuring Data/Dictionaries.py
@@ -0,0 +1,32 @@
+
+
+spam = {'name': 'Pooka', 'age': 5}
+spam.setdefault('color', 'black')
+print(spam)
+
+# 漂亮打印
+import pprint
+message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
+count = {}
+for character in message:
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+pprint.pprint(count)
+
+# 嵌套的字典和列表
+allGuests = {   'Alice' : {'apples': 5, 'pretzels': 12},
+                'Bob'   : {'ham sandwiches': 3, 'apples': 2},
+                'Carol' : {'cups': 3, 'apple pies': 1}}
+
+def totalBrought(guests, item):
+    numBrought = 0
+    for k, v in guests.items():
+        numBrought = numBrought + v.get(item, 0)
+    return numBrought
+
+print('Number of things being brought:')
+print(' - Apples ' + str(totalBrought(allGuests, 'apples')))
+print(' - Cups ' + str(totalBrought(allGuests, 'cups')))
+print(' - Cakes ' + str(totalBrought(allGuests, 'cakes')))
+print(' - Ham Sandwiches ' + str(totalBrought(allGuests, 'ham sandwiches')))
+print(' - Apple Pies ' + str(totalBrought(allGuests, 'apple pies')))
